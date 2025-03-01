e5603cc81c3fa5fbc47b6f1d0186aa47164f2571
f1avour <iced.flavour@gmail.com>
2025-03-01 10:54:58 +0200

    Fixed Destroying Floors

--- Assets/script/Destroyer.cs
+++ Assets/script/Destroyer.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LvlNO : MonoBehaviour
+public class Destroyer : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball")){
-            transform.parent.gameObject.SetActive(false);
+            transform.gameObject.SetActive(false);
         }
     }
 }
