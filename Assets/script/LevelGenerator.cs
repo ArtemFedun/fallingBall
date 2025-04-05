8e832af03aca3f6649a8ca190e84453d65bc6aa0
f1avour <iced.flavour@gmail.com>
2025-04-05 11:04:49 +0300

    Created base structure with index error

--- Assets/script/LevelGenerator.cs
+++ Assets/script/LevelGenerator.cs
@@ -18,17 +18,21 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private int SegmentsNumber;
     [SerializeField] private int AngleDiff;
     [SerializeField] private int LevelDifficulty;
+    [SerializeField] private int GlassStreak;
+    [SerializeField] private int KillStreak;
 
     private int GapDifference;
     private int GapDifficulty;
     private int GlassDifficulty;
     private int KillDifficulty;
 
+    private int[] totalIndexes;
 
     private void Start()
     {
         ChangeDifficulty(LevelDifficulty);
         GenerateLevel();
+        totalIndexes = new int[GapDifficulty + GlassDifficulty + KillDifficulty];
     }
 
     private void ChangeDifficulty(int levelDifficulty)
@@ -45,25 +49,25 @@ public class LevelGenerator : MonoBehaviour
                 GapDifference   = 4;
                 GapDifficulty   = 5;
                 GlassDifficulty = 0;
-                KillDifficulty  = 2;
+                KillDifficulty  = 1;
                 break;
             case 3:
                 GapDifference   = 6;
                 GapDifficulty   = 4;
-                GlassDifficulty = 2;
-                KillDifficulty  = 4;
+                GlassDifficulty = 1;
+                KillDifficulty  = 2;
                 break;
             case 4:
                 GapDifference   = 8;
                 GapDifficulty   = 4;
-                GlassDifficulty = 4;
-                KillDifficulty  = 6;
+                GlassDifficulty = 2;
+                KillDifficulty  = 3;
                 break;
             case 5:
                 GapDifference   = 10;
                 GapDifficulty   = 3;
-                GlassDifficulty = 6;
-                KillDifficulty  = 8;
+                GlassDifficulty = 3;
+                KillDifficulty  = 4;
                 break;
         }
     }
@@ -92,20 +96,23 @@ public class LevelGenerator : MonoBehaviour
         //********************* РОЗРАХУНОК GAPS *********************//
             int[] gapIndexes = new int[GapDifficulty];
             GenerateGaps(gapBasis, gapIndexes);
+            totalIndexes = gapIndexes.ToArray();
         //**********************************************************//
-
+            
         //********************** РОЗРАХУНОК KILLBLOCKS **********************//
             int[] killIndexes = new int[KillDifficulty];
             var killBegin = (gapBasis + GapDifficulty) % SegmentsNumber;
             GenerateKill(killIndexes, killBegin);
+            totalIndexes = gapIndexes.Concat(killIndexes).ToArray();
         //*****************************************************************//
-        
+            
         //********************** РОЗРАХУНОК GLASSBLOCKS **********************/
             int[] glassIndexes = new int[GlassDifficulty];
             var glassBegin = (gapBasis + GapDifficulty) % SegmentsNumber;
-            GenerateGlass(glassIndexes, glassBegin, killIndexes);
+            GenerateGlass(glassIndexes, glassBegin);
+            totalIndexes = gapIndexes.Concat(killIndexes).Concat(glassIndexes).ToArray();
         //*****************************************************************//
-
+            
         //********************** ЗАПОВНЕННЯ ПОВЕРХУ **********************//
             for (int segmentIndex = 0; segmentIndex < SegmentsNumber; segmentIndex++)
             {
@@ -137,45 +144,6 @@ public class LevelGenerator : MonoBehaviour
         SegmentInstance.GetComponent<Segment>().Index = index;
     }
 
-    private void GenerateGaps(int gapBasis, int[] gapIndexes){
-        gapBasis = (gapBasis + Random.Range(-GapDifference, GapDifference)) % SegmentsNumber;
-
-        for (int index = 0; index < gapIndexes.Length; index++)
-        {
-            gapIndexes[index] = (gapBasis + index) % SegmentsNumber;
-        }
-    }
-
-    private void GenerateKill(int[] killIndexes, int killBegin){
-
-        for (int j = 0; j < KillDifficulty; j++)
-        {
-            var killIndex = (killBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
-
-            while (killIndexes.Contains(killIndex))
-            {
-                killIndex = (killBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
-            }
-
-            killIndexes[j] = killIndex;
-        }
-    }
-
-    private void GenerateGlass(int[] glassIndexes, int glassBegin, int[] killIndexes){
-
-        for (int j = 0; j < GlassDifficulty; j++)
-        {
-            var glassIndex = (glassBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
-            
-            while (glassIndexes.Contains(glassIndex) || killIndexes.Contains(glassIndex))
-            {
-                glassIndex = (glassBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
-            }
-
-            glassIndexes[j] = glassIndex;
-        }
-    }
-
     private void SpawnFinish(GameObject levelObject)
     {
         var floorSpawnPosition = new Vector3(0.0f, -FloorsNumber * FloorsGap, 0.0f);
@@ -185,5 +153,64 @@ public class LevelGenerator : MonoBehaviour
         {
             SpawnSegment(FinishSegment, floorSpawnPosition, FinishInstance, segmentIndex);
         }
+    }
+
+    private void GenerateGaps(int gapBasis, int[] gapIndexes)
+    {
+        gapBasis = (gapBasis + Random.Range(-GapDifference, GapDifference)) % SegmentsNumber;
+
+        for (int index = 0; index < gapIndexes.Length; index++)
+        {
+            gapIndexes[index] = (gapBasis + index) % SegmentsNumber;
+        }
+    }
+
+    private void GenerateKill(int[] killIndexes, int killBegin)
+    {
+        for (int index = 0; index < KillDifficulty; index++)
+        {
+            var killIndex = (killBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
+
+            while (CheckIndex(killIndex, KillStreak, totalIndexes))
+            {
+                killIndex = (killIndex + 1)  % SegmentsNumber;
+            }
+
+            for (int killStreak = 0; killStreak < KillStreak; killStreak++)
+            {
+                killIndexes[index + killStreak] = (killIndex + killStreak) % SegmentsNumber;
+            }
+ 
+        }
+    }
+
+    private void GenerateGlass(int[] glassIndexes, int glassBegin){
+
+        for (int index = 0; index < GlassDifficulty; index++)
+        {
+            var glassIndex = (glassBegin + Random.Range(0, SegmentsNumber - GapDifficulty)) % SegmentsNumber;
+            
+            while (CheckIndex(glassIndex, GlassStreak, totalIndexes))
+            {
+                glassIndex = (glassIndex + 1)  % SegmentsNumber;
+            }
+
+            for (int glassStreak = 0; glassStreak < GlassStreak; glassStreak++)
+            {
+                glassIndexes[index + glassStreak] = (glassIndex + glassStreak) % SegmentsNumber;
+            }
+        }
+    }
+
+    private bool CheckIndex(int checkIndex, int streak, int[] indexesArray)
+    {
+        for (int index = 0; index < streak; index++)
+        {
+            if (indexesArray.Contains(checkIndex + index))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
