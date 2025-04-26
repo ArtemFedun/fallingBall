11ce9b1b18f39feb20d527cec7f189251d7ab6fe
Artem <artem2011.fedun@gmail.com>
2025-04-26 10:03:45 +0300

    trail

--- Assets/script/Score.cs
+++ Assets/script/Score.cs
@@ -6,17 +6,18 @@ public class Score : MonoBehaviour
 {
     float score = 0;
     float scoreAdd = 2;
-    float streak = 1;
-    private void OnTriggerEnter(Collider other)
+    float streak = 0;
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Pass")){
-            streak++;
-            score = scoreAdd;
+            score = score + scoreAdd;
             scoreAdd = scoreAdd * streak;
+            streak++;
             Debug.Log(score);
-        } else if(other.CompareTag("Ground") || other.CompareTag("Glass") || other.CompareTag("Kill")){
-            streak = 1;
-            scoreAdd = 12;
+        }
+        if(other.CompareTag("Ground") || other.CompareTag("Glass") || other.CompareTag("Kill")){
+            streak = 0;
+            scoreAdd = 2;
         }
     }
 }
